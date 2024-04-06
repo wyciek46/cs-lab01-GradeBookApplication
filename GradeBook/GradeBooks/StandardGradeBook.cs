b8d5b475e5e84431e73881c981e3e49c7197055a
wyciek46 <wiktor.szydlowski@microsoft.wsei.edu.pl>
2024-04-06 18:11:07 +0200

    Creating StandardGradeBook Class

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,12 @@
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
