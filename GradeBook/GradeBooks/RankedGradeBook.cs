0e3f2b2c75a18026ab258f338b63264b8a1a2887
wyciek46 <wiktor.szydlowski@microsoft.wsei.edu.pl>
2024-04-06 18:27:50 +0200

    Creating CalculateStudentStatistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,6 +10,16 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+          public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
+        }
          public override void CalculateStatistics()
         {
             if (Students.Count >= 5)
