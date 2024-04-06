b97ba919aa7af9c8c8a69ecbd0575ff22b65bf94
wyciek46 <wiktor.szydlowski@microsoft.wsei.edu.pl>
2024-04-06 18:16:24 +0200

    Creating RankedGradeBook class

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,12 @@
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
