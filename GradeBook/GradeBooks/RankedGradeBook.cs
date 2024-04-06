2d406a536c4cdea973f7cce592a0c78e1f4aa9ac
wyciek46 <wiktor.szydlowski@microsoft.wsei.edu.pl>
2024-04-06 18:22:44 +0200

    Creating GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 using GradeBook.Enums;
+using System.Linq;
+using System;
 
 namespace GradeBook.GradeBooks
 {
@@ -8,5 +10,27 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+         public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Less than 5 Students");
+            
+            var Hold = (int)Math.Ceiling(Students.Count * 0.2);
+
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades.IndexOf(averageGrade) < Hold)
+                return 'A';
+            else if (grades.IndexOf(averageGrade) < Hold * 2)
+                return 'B';
+            else if (grades.IndexOf(averageGrade) < Hold * 3)
+                return 'C';
+            else if (grades.IndexOf(averageGrade) < Hold * 4)
+                return 'D';
+            else
+                return 'F';
+        }
     }
 }
+    
+
